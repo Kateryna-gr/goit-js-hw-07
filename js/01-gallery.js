1c4ad8d24f365f0001c42c4a6ba1655bc32f3907
Kateryna-gr <kategrytsyk@gmail.com>
2023-08-03 23:03:52 +0300

    task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const gallery = document.querySelector(".js-gallery");
+
+const markup = galleryItems
+  .map(({ preview, description }, idx) => `<li class="gallery__item">
+    <img class="gallery__image" data-id="${idx}" src="${preview}" alt="${description}">
+  </li>`
+  ).join("");
+
+gallery.insertAdjacentHTML("beforeend", markup);
+
+gallery.addEventListener("click", handlerClick);
+
+function handlerClick(evt) {
+  if (evt.currentTarget === evt.target) {
+    return;
+  }
+
+  basicLightbox.create(`<img src="${galleryItems[evt.target.dataset.id].original}" alt="">`)
+    .show();
+}
