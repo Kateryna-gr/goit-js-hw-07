3cd57953e529f5c911eddaf888e051bc0195ea6a
Kateryna-gr <kategrytsyk@gmail.com>
2023-08-03 21:30:57 +0300

    start

--- js/task-01.js
+++
@@ -1,9 +0,0 @@
-const items = document.querySelector("#categories").children;
-console.log(`Number of categories: ${items.length}`);
-
-const itemsCopy = [...items];
-
-itemsCopy.forEach((item) => {
-  console.log(`Category: ${item.querySelector("h2").textContent}`);
-  console.log(`Elements: ${item.querySelectorAll("li").length}`);
-});
--- js/task-07.js
+++
@@ -1,8 +0,0 @@
-const elem = {
-  input: document.querySelector("#font-size-control"),
-  span: document.querySelector("#text"),
-};
-
-elem.input.addEventListener("input", () => {
-  elem.span.style.fontSize = `${elem.input.value}px`;
-});
